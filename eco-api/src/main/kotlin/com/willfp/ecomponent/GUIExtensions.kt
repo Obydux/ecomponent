b3e811937ef04b6d642dda4cba0905671e82a417
Auxilor <william.favierparsons1@gmail.com>
2022-11-07 14:57:31 +0000

    Removed SlotProvider

--- eco-api/src/main/kotlin/com/willfp/ecomponent/GUIExtensions.kt
+++ eco-api/src/main/kotlin/com/willfp/ecomponent/GUIExtensions.kt
@@ -9,22 +9,14 @@ import org.bukkit.event.inventory.InventoryClickEvent
 /** An action when clicking a slot. */
 typealias SlotAction = (Player, event: InventoryClickEvent, Slot, Menu) -> Unit
 
-/** Creating a slot given a player and a menu. */
-typealias SlotProvider = (Player, Menu) -> Slot?
-
 private class OrElseGUIComponent(
     private val delegate: GUIComponent,
-    private val slotProvider: SlotProvider
+    private val slot: Slot?
 ) : GUIComponent by delegate {
     override fun getSlotAt(row: Int, column: Int, player: Player, menu: Menu): Slot? =
-        delegate.getSlotAt(row, column, player, menu) ?: slotProvider(player, menu)
+        delegate.getSlotAt(row, column, player, menu) ?: slot
 }
 
-/** Convert a slot to a slot provider. */
-internal fun Slot?.toSlotProvider(): SlotProvider = { _, _ -> this }
 
 /** Show this GUIComponent or default to another slot if null. */
-fun GUIComponent.orElse(slot: Slot?): GUIComponent = orElse(slot.toSlotProvider())
-
-/** Show this GUIComponent or default to another slot if null. */
-fun GUIComponent.orElse(slot: SlotProvider): GUIComponent = OrElseGUIComponent(this, slot)
+fun GUIComponent.orElse(slot: Slot?): GUIComponent = OrElseGUIComponent(this, slot)
--- eco-api/src/main/kotlin/com/willfp/ecomponent/components/Shapes.kt
+++ eco-api/src/main/kotlin/com/willfp/ecomponent/components/Shapes.kt
@@ -4,15 +4,13 @@ import com.willfp.eco.core.gui.GUIComponent
 import com.willfp.eco.core.gui.menu.Menu
 import com.willfp.eco.core.gui.slot.Slot
 import com.willfp.ecomponent.AutofillComponent
-import com.willfp.ecomponent.SlotProvider
-import com.willfp.ecomponent.toSlotProvider
 import org.bukkit.entity.Player
 
 /** A line of identical slots. */
 private class LineComponent(
     private val direction: LineDirection,
     private val maxSize: Int,
-    private val slotProvider: SlotProvider
+    private val slot: Slot?
 ) : AutofillComponent() {
     override fun getColumns() =
         if (direction == LineDirection.HORIZONTAL) maxColumns.coerceAtMost(maxSize) else 1
@@ -21,7 +19,7 @@ private class LineComponent(
         if (direction == LineDirection.VERTICAL) maxRows.coerceAtMost(maxSize) else 1
 
     override fun getSlotAt(row: Int, column: Int, player: Player, menu: Menu) =
-        slotProvider(player, menu)
+        slot
 }
 
 /** A direction for a line to go in. */
@@ -31,59 +29,40 @@ private enum class LineDirection {
 }
 
 /*
-
 Vertical Lines.
-
  */
+
 
 /**
  * Create a vertical line of identical [slot]s. Takes the maximum height
  * possible.
  */
-fun verticalLine(slot: Slot): GUIComponent = verticalLine(Int.MAX_VALUE, slot)
+fun verticalLine(slot: Slot?): GUIComponent = verticalLine(Int.MAX_VALUE, slot)
 
 /** Create a vertical line of identical [slot]s, with a [maxSize]. */
-fun verticalLine(maxSize: Int, slot: Slot): GUIComponent = verticalLine(maxSize, slot.toSlotProvider())
+fun verticalLine(maxSize: Int, slot: Slot?): GUIComponent = LineComponent(LineDirection.VERTICAL, maxSize, slot)
 
-/**
- * Create a vertical line of identical [slot]s. Takes the maximum height
- * possible.
- */
-fun verticalLine(slot: SlotProvider): GUIComponent = LineComponent(LineDirection.VERTICAL, Int.MAX_VALUE, slot)
-
-/** Create a vertical line of identical [slot]s, with a [maxSize]. */
-fun verticalLine(maxSize: Int, slot: SlotProvider): GUIComponent = LineComponent(LineDirection.VERTICAL, maxSize, slot)
 
 /*
-
 Horizontal Lines.
-
  */
+
 
 /**
  * Create a horizontal line of identical [slot]s. Takes the maximum height
  * possible.
  */
-fun horizontalLine(slot: Slot): GUIComponent = horizontalLine(Int.MAX_VALUE, slot)
+fun horizontalLine(slot: Slot?): GUIComponent = horizontalLine(Int.MAX_VALUE, slot)
 
 /** Create a horizontal line of identical [slot]s, with a [maxSize]. */
-fun horizontalLine(maxSize: Int, slot: Slot): GUIComponent = horizontalLine(maxSize, slot.toSlotProvider())
-
-/**
- * Create a horizontal line of identical [slot]s. Takes the maximum height
- * possible.
- */
-fun horizontalLine(slot: SlotProvider): GUIComponent = LineComponent(LineDirection.HORIZONTAL, Int.MAX_VALUE, slot)
-
-/** Create a horizontal line of identical [slot]s, with a [maxSize]. */
-fun horizontalLine(maxSize: Int, slot: SlotProvider): GUIComponent =
+fun horizontalLine(maxSize: Int, slot: Slot?): GUIComponent =
     LineComponent(LineDirection.HORIZONTAL, maxSize, slot)
 
+
 /*
-
 Rectangles.
-
  */
+
 
 /**
  * Create a rectangle of identical [slot]s. Fills as much space as
@@ -95,23 +74,10 @@ fun rectangle(slot: Slot): GUIComponent = rectangle(Int.MAX_VALUE, Int.MAX_VALUE
  * Create a rectangle of identical [slot]s. Fills up to a [maxHeight] and
  * [maxWidth].
  */
-fun rectangle(maxHeight: Int, maxWidth: Int, slot: Slot): GUIComponent =
-    rectangle(maxHeight, maxWidth, slot.toSlotProvider())
-
-/**
- * Create a rectangle of identical [slot]s. Fills as much space as
- * possible.
- */
-fun rectangle(slot: SlotProvider): GUIComponent = rectangle(Int.MAX_VALUE, Int.MAX_VALUE, slot)
-
-/**
- * Create a rectangle of identical [slot]s. Fills up to a [maxHeight] and
- * [maxWidth].
- */
-fun rectangle(maxHeight: Int, maxWidth: Int, slot: SlotProvider): GUIComponent = object : AutofillComponent() {
+fun rectangle(maxHeight: Int, maxWidth: Int, slot: Slot): GUIComponent = object : AutofillComponent() {
     override fun getColumns() = maxColumns.coerceAtMost(maxWidth)
 
     override fun getRows() = maxRows.coerceAtMost(maxHeight)
 
-    override fun getSlotAt(row: Int, column: Int, player: Player, menu: Menu) = slot(player, menu)
+    override fun getSlotAt(row: Int, column: Int, player: Player, menu: Menu) = slot
 }
