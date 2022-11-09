28d92b1e22759560fe16c2e2cc0522a23ea6df9f
Auxilor <william.favierparsons1@gmail.com>
2022-11-09 14:17:38 +0000

    Added MenuStateVar

---
+++ eco-api/src/main/kotlin/com/willfp/ecomponent/MenuStateVar.kt
@@ -0,0 +1,16 @@
+package com.willfp.ecomponent
+
+import com.willfp.eco.core.gui.menu.Menu
+import org.bukkit.entity.Player
+
+/** Delegate menu state to a fixed variable. */
+class MenuStateVar<T>(
+    private val menu: Menu,
+    private val key: String
+) {
+    operator fun get(player: Player): T? =
+        menu.getState(player, key)
+
+    operator fun set(player: Player, value: T?) =
+        menu.setState(player, key, value)
+}
