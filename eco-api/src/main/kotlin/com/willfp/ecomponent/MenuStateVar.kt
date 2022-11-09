64ecf4839e22ccaa57a3e5648ec5de2899f2367f
Auxilor <william.favierparsons1@gmail.com>
2022-11-09 14:22:31 +0000

    Updated MenuStateVar

--- eco-api/src/main/kotlin/com/willfp/ecomponent/MenuStateVar.kt
+++ eco-api/src/main/kotlin/com/willfp/ecomponent/MenuStateVar.kt
@@ -3,14 +3,33 @@ package com.willfp.ecomponent
 import com.willfp.eco.core.gui.menu.Menu
 import org.bukkit.entity.Player
 
-/** Delegate menu state to a fixed variable. */
-class MenuStateVar<T : Any>(
+/** Delegates menu state to a fixed variable. */
+interface MenuStateVar<T> {
+    operator fun get(player: Player): T
+    operator fun set(player: Player, value: T)
+}
+
+/** Menu state variable with null as default. */
+class NullableMenuStateVar<T : Any>(
     private val menu: Menu,
     private val key: String
-) {
-    operator fun get(player: Player): T? =
+) : MenuStateVar<T?> {
+    override operator fun get(player: Player): T? =
         menu.getState(player, key)
 
-    operator fun set(player: Player, value: T?) =
+    override operator fun set(player: Player, value: T?) =
+        menu.setState(player, key, value)
+}
+
+/** Menu state variable with a fixed default. */
+class NotNullMenuStateVar<T : Any>(
+    private val menu: Menu,
+    private val key: String,
+    private val default: T
+) : MenuStateVar<T> {
+    override operator fun get(player: Player): T =
+        menu.getState(player, key) ?: default
+
+    override operator fun set(player: Player, value: T) =
         menu.setState(player, key, value)
 }
